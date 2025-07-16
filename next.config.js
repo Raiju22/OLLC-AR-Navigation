7d1136b428816876f7b7fdd0135c502425f81ea7
Raiju22 <technoblade0100@gmail.com>
2025-07-16 09:35:15 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/OLLC-AR-Navigation', // Adjust to match your GitHub repo name
+};
+
+module.exports = nextConfig;
